e61100b2b133b1157fa433a7b104eaae2c2d22ce
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-20 11:36:27 +0000

    reupload

---
+++ htmlChallenges.js
@@ -0,0 +1,19 @@
+
+
+// let reverseString = (str) => {
+//     splitString = str.split("");
+//     let reverse = splitString.reverse();
+//     let reversedAndJoined = reverse.join(""); 
+//     return reversedAndJoined;
+// }
+
+// console.log(reverseString("Hello"));
+
+let reverseString = (str) => {return str.split("").reverse().join("");}
+console.log(reverseString("Hello"));
+
+
+
+
+
+
